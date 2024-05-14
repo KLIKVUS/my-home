e8e3434ad469710a9a0002125efc047414eb934b
admin <this.mail.for.mail@gmail.com>
2024-05-14 11:31:07 +0300

    хз что я тут менял

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,8 +7,12 @@ import './index.scss';
 function Nav({ panelIcon }) {
     const location = useLocation();
     const [linkSelectorWidth, setLinkSelectorWidth] = useState();
-
     const [panelIsActive, setPanelIsActive] = useState(false);
+
+    const handelPanelActivation = () => {
+        document.getElementsByTagName("body")[0].style.overflowY = !panelIsActive ? "hidden" : "auto";
+        setPanelIsActive(!panelIsActive);
+    }
 
     useEffect(() => {
         const elem1 = document.querySelector(".panel__link-selector");
@@ -22,38 +26,40 @@ function Nav({ panelIcon }) {
     }, [location])
 
     return (
-        <div className={panelIsActive ? "panel panel--open" : "panel"}>
-            <div className="panel__icons-wrapper">
-                <div className="panel__icon">
-                    <div className="panel__page-icon" style={panelIcon && { backgroundImage: `url(/icons/page/${panelIcon})` }} />
+        <div className={panelIsActive ? "panel-wrapper--open" : "panel-wrapper"}>
+            <div className="panel">
+                <div className="panel__icons-wrapper">
+                    <div className="panel__icon">
+                        <div className="panel__page-icon" style={panelIcon && { backgroundImage: `url(/icons/page/${panelIcon})` }} />
+                    </div>
+
+                    <div className="panel__icon panel__btn-icon">
+                        <button className={["panel__btn", "btn", panelIsActive ? "btn--close" : null].join(" ")} onClick={handelPanelActivation}>
+                            <span className="btn__line" />
+                            <span className="btn__line" />
+                        </button>
+                    </div>
                 </div>
 
-                <div className="panel__icon panel__btn-icon">
-                    <button className={["panel__btn", "btn", panelIsActive ? "btn--close" : null].join(" ")} onClick={() => setPanelIsActive(!panelIsActive)}>
-                        <span className="btn__line" />
-                        <span className="btn__line" />
-                    </button>
+                <div className="panel__links-wrapper">
+                    <div style={linkSelectorWidth ? { width: linkSelectorWidth + 20 + "px" } : null} className={"panel__link-selector"}>
+                        <NavLink
+                            to="/"
+                            className={({ isActive }) => isActive ? "" : "panel__back-link"}
+                        />
+                    </div>
+
+                    <nav className="panel__nav nav">
+                        <NavLink to="/works" className="nav__link" onClick={handelPanelActivation}>Works</NavLink>
+                        <NavLink to="/feedbacks" className="nav__link" onClick={handelPanelActivation}>Feedbacks</NavLink>
+                        <NavLink to="/about" className="nav__link" onClick={handelPanelActivation}>About</NavLink>
+                    </nav>
                 </div>
-            </div>
 
-            <div className="panel__links-wrapper">
-                <div style={linkSelectorWidth ? { width: linkSelectorWidth + 20 + "px" } : null} className={"panel__link-selector"}>
-                    <NavLink
-                        to="/"
-                        className={({ isActive }) => isActive ? "" : "panel__back-link"}
-                    />
+                <div className="panel__social social">
+                    <NavLink to="https://vk.com/aappi" target="_blank" className="social__link social__link-img--vk" />
+                    <NavLink to="https://t.me/KLIKVUS" target="_blank" className="social__link social__link-img--tg" />
                 </div>
-
-                <nav className="panel__nav nav">
-                    <NavLink to="/works" className="nav__link">Works</NavLink>
-                    <NavLink to="/feedbacks" className="nav__link">Feedbacks</NavLink>
-                    <NavLink to="/about" className="nav__link">About</NavLink>
-                </nav>
-            </div>
-
-            <div className="panel__social social">
-                <NavLink to="https://vk.com/aappi" target="_blank" className="social__link social__link-img--vk" />
-                <NavLink to="https://t.me/KLIKVUS" target="_blank" className="social__link social__link-img--tg" />
             </div>
         </div>
     );
